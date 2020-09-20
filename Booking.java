7cf97d721b6c2c3f929d6faa56adeb7adc3af74e
nf-mach-1 <61986148+nf-mach-1@users.noreply.github.com>
2020-09-21 00:21:49 +0200

    Update Booking.java
    created a gui interface using java

--- Booking.java
+++ Booking.java
@@ -12,6 +12,7 @@ public class Booking extends JFrame implements ActionListener{
 	private static JTextArea msgbox;
 	private static JTextField cName;
 	private static JTextField cNumber;
+	//combo box values
 	String[] days = {"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31"};
 	JComboBox dbox = new JComboBox(days);
 	String[] months = {"January","February","March","April","May","June","July","August","September","October","November","December"};
