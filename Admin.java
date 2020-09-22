2c0942ef6f13842dae11404657f8a0805d852b58
nf-mach-1 <61986148+nf-mach-1@users.noreply.github.com>
2020-09-22 14:49:26 +0200

    Update Admin.java
    Admin can now view bookings

--- Admin.java
+++ Admin.java
@@ -1,19 +1,25 @@
 import javax.swing.*;
 
 import java.awt.FlowLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.*;
 import java.util.*;
 
 public class Admin extends JFrame{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private JTextArea view;
 	private JLabel elbl = new JLabel(); 
+	JTable table;
 	public Admin() {
-		setSize(750,500); // setting the size of the window
+		setSize(650,200); // setting the size of the window
 		setTitle("View Bookings"); 
-		
 		JPanel mainpanel = new JPanel();
-		JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-		view = new JTextArea(85,5);
+		JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEFT));;
+		view = new JTextArea(100,10);
 		try {
 			Scanner sc = new Scanner(new File("booking.txt"));
 			String line;
@@ -21,11 +27,23 @@ public class Admin extends JFrame{
 				line = sc.nextLine();
 				view.append(line+"\n");
 			}
+			sc.close();
 		}catch(Exception e) {
 			elbl.setText(e.getMessage());
 		}
 		panel.add(view);
 		panel.add(elbl);
+		
+		JPanel bpanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+		JButton exit = new JButton("Exit");
+		bpanel.add(exit);
+		exit.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				System.exit(0);
+			}
+		});
+		
+		mainpanel.add(bpanel);
 		mainpanel.add(panel);
 		add(mainpanel);
 	}
