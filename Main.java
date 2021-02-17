62ce54bc4df0133a82e2dc0b60f39b9904b58342
Nhlakanipho Machi <61986148+nf-mach-1@users.noreply.github.com>
2021-02-17 14:08:55 +0200

    Update Main.java
    Requires login details from the users

--- Main.java
+++ Main.java
@@ -9,35 +9,48 @@ public class Main extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+	
 	public Main() {
-		setSize(400,100); 
+		setSize(400,300); 
 		setTitle("Appointments Booking Application"); 
 		JPanel mainpanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
 		JLabel welc = new JLabel();
 		welc.setText("Welcome to the booking application.");
 		mainpanel.add(welc);
+		
+		//user details prompt
+		JLabel username = new JLabel("Username :");
+		JTextField txtuser = new JTextField(20);
+		JPanel field = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		JLabel password = new JLabel("Password :");
+		JTextField txtpass = new JTextField(20);
+		JPanel field2 = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		field.add(username);
+		field.add(txtuser);
+		field2.add(password);
+		field2.add(txtpass);
+		mainpanel.add(field);
+		mainpanel.add(field2);
+		
 		JPanel btn = new JPanel(new FlowLayout(FlowLayout.LEFT));
-		JButton adm = new JButton("View Booking");
-		adm.addMouseListener(new MouseAdapter() {
+		JButton login = new JButton("Login");
+		btn.add(login);
+		
+		login.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent e) {
-				Admin admin = new Admin();
-				admin.setVisible(true);
-				mWin.setVisible(false);
+				if(txtuser.getText().equals("admin@work.com") && txtpass.getText().equals("admin")) {
+					Admin admin = new Admin();
+					admin.setVisible(true);
+					mWin.setVisible(false);
+				}else if(txtuser.getText().equals("test@work.com") && txtpass.getText().equals("test")) {
+					Booking window1 = new Booking();
+					window1.setVisible(true);
+					mWin.setVisible(false);
+				}else {
+					JOptionPane.showMessageDialog(rootPane, "ERROR!! Enter valid login details.");
+				}
 			}
 		});
-		
-		btn.add(adm);
-		
-		JButton book = new JButton("Booking");
-		book.addMouseListener(new MouseAdapter() {
-			public void mouseClicked(MouseEvent e) {
-				Booking window1 = new Booking();
-				window1.setVisible(true);
-				mWin.setVisible(false);
-			}
-		});
-		btn.add(book);
 		
 		JButton exit = new JButton("Exit");
 		exit.addMouseListener(new MouseAdapter() {
